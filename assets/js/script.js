1b50327d5fc5283467849683f6ad55afcce49fe7
Scott Bradley <scottabradley91@gmail.com>
2021-10-10 20:13:56 -0400

    Established variables for fetching weather api info

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    // Search variables
+    const searchBtn = $("#button-search");
+    let searchTerm = $("#search-term");
+    let pastSearch = $("#past-search");
+    let searchCity = "";
+    const clearBtn = $("#clear-search");
+
+    // Current weather variables
+    const cityHeader = $("#city-date");
+    const cityIcon = $("#weather-icon-current");
+    const cityTemp = $("#city-temp");
+    const cityHumidity = $("#city-humidity");
+    const cityWindSpeed = $("#city-wind-speed");
+    const cityUVIndex = $("#city-uv-index");
+
+    // Moment
+    const todaysDate = moment();
+
+    // Current weather function
+})
