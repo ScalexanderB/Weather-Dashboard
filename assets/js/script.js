a4dd2f3398a45279dcad82e3c92cb1535c967d82
Scott Bradley <scottabradley91@gmail.com>
2021-10-10 20:48:02 -0400

    created functions to call API, and to display and color code UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,67 @@ $(document).ready(function () {
     const todaysDate = moment();
 
     // Current weather function
+    function buildCurrentQueryURL() {
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?";
+
+        let queryParams = { "APPID": "77672c68786de792de20e4e44617bd62" };
+
+        queryParams.q = searchTerm
+            .val()
+            .trim();
+        
+        return queryURL + $.param(queryParams);
+    }
+
+    // Display search results based on API
+    function updateCurrentWeather(response) {
+        //display weather icon
+        let weatherIcon = response.weather[0].icon;
+        let weatherIconURL = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
+        let weatherIconDescription = response.weather[0].description;
+        // Temp: Convert to fahrenheit
+        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        searchCity = response.name;
+
+        cityHeader.text(`${searchCity} (${todaysDate.format("MM/DD/YYYY")})`);
+        cityHeader.append(cityIcon.attr("src", weatherIconURL).attr("alt", `${weatherIconDescription}`).attr("title", `${weatherIconDescription}`));
+        cityTemp.text(`Temperature: ${tempF.toFixed(2)} ℉`);
+        cityHumidity.text(`Humidity: ${response.main.humidity}%`);
+        cityWindSpeed.text(`Wind Speed: ${response.wind.speed} MPH`);
+
+        //UV Index
+        let currentLat = response.coord.lat;
+        let currentLong = response.coord.lon;
+        let uvQueryURL = `https://api.openweathermap.org/data/2.5/uvi?appid=77672c68786de792de20e4e44617bd62&lat=${currentLat}&lon=${currentLong}`;
+
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"
+        })
+
+            .then(function (response) {
+                let uvValue = response.value;
+                cityUVIndex.text(`UV Index: `);
+                let uvSpan = $("<span>").text(uvValue).addClass("p-2");
+
+                if (uvValue >= 0 && uvValue < 3) {
+                    uvSpan.addClass("green");
+                }
+                else if (uvValue >= 3 && uvValue < 6) {
+                    uvSpan.addClass("yellow");
+                }
+                else if (uvValue >= 6 && uvValue < 8) {
+                    uvSpan.addClass("orange");
+                }
+                else if (uvValue >= 8 && uvValue < 11) {
+                    uvSpan.addClass("red");
+                }
+                else if (uvValue >= 11) {
+                    uvSpan.addClass("black");
+                }
+
+                cityUVIndex.append(uvSpan);
+            })
+    }
 })
